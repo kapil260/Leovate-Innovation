f6993a72f40493fb9a48fa44841a1c2418fcbdf7
Shrawan Deuba <shrawandeuba27@gmail.com>
2026-06-14 18:06:14 +0545

    Update signup.js

--- signup.js
+++ signup.js
@@ -5,7 +5,7 @@
 
 'use strict';
 
-const BACKEND_URL = 'http://localhost:5000';
+const BACKEND_URL = 'https://leovate-innovation-3.onrender.com';
 const API = {
   sendOtp:   `${BACKEND_URL}/api/auth/signup-send-otp`,
   verifyOtp: `${BACKEND_URL}/api/auth/signup-verify`,
