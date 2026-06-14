8ab0a870eda4ab33d1ce3b11fa4af1a45d977b72
Shrawan Deuba <shrawandeuba27@gmail.com>
2026-06-14 18:05:39 +0545

    Change backend URL to production server

--- login.js
+++ login.js
@@ -5,7 +5,7 @@
 
 'use strict';
 
-const BACKEND_URL = 'http://localhost:5000';
+const BACKEND_URL = 'https://leovate-innovation-3.onrender.com';
 const API = { signin: `${BACKEND_URL}/api/auth/login` };
 
 const dom = {
